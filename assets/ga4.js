54dabb14a08f191df70a1fb0be6f147a407c4b2e
Your Name <your.email@example.com>
2026-06-27 00:15:41 +0800

    Add GA4 tracking tag

---
+++ assets/ga4.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", "G-NWHFZH1Z7E");
+
+const gaScript = document.createElement("script");
+gaScript.async = true;
+gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-NWHFZH1Z7E";
+document.head.appendChild(gaScript);
